f83ded9ebd8a7a6e74856fb2f1310072d74132d3
Philipp Jardas <philipp.jardas@qualifyze.com>
2022-01-15 12:08:08 +0100

    (website) Logo und Favicon hinzugefügt

--- packages/website/gatsby-config.js
+++ packages/website/gatsby-config.js
@@ -1,6 +1,17 @@
 module.exports = {
-  siteMetadata: {
-    title: "Taktische Zeichen",
-  },
-  plugins: ["gatsby-plugin-netlify"],
+  plugins: [
+    "gatsby-plugin-netlify",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Taktische Zeichen",
+        short_name: "Taktische Zeichen",
+        start_url: "/",
+        background_color: "#f5f5f5",
+        theme_color: "#0d6efd",
+        display: "standalone",
+        icon: "src/assets/logo.svg",
+      },
+    },
+  ],
 };
