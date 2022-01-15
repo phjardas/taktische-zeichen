6c582793cf305d07a8164c2cf7451f88d5c8d7dd
Philipp Jardas <philipp.jardas@qualifyze.com>
2022-01-15 12:07:55 +0100

    (website) Hellgrauer Hintergrund

--- packages/website/src/components/Layout.js
+++ packages/website/src/components/Layout.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Nav from "./Nav";
+import "./styles.css";
+import logo from "../assets/logo.svg";
 
 export default function Layout({ children }) {
   return (
     <div className="container py-3">
-      <h1 className="display-1">Taktische Zeichen</h1>
+      <h1 className="display-1 d-flex align-items-center">
+        <img src={logo} alt="Logo" className="me-3" style={{ height: "1em" }} />
+        Taktische Zeichen
+      </h1>
       <Nav />
       <main className="mt-3">{children}</main>
     </div>
