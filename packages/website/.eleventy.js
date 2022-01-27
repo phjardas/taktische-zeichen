6c8fa573afe9d2f906529c19da1d1731297d25ad
Philipp Jardas <philipp.jardas@qualifyze.com>
2022-01-27 14:33:41 +0100

    (website) add syntax highlighting

--- packages/website/.eleventy.js
+++ packages/website/.eleventy.js
@@ -1,4 +1,7 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPassthroughCopy("static");
   eleventyConfig.addPassthroughCopy("compiled");
   eleventyConfig.addPassthroughCopy("previews");
