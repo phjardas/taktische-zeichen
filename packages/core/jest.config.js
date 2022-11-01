ce2159f3edb7c4b5c650c9b09c299e7499dac07c
Philipp Jardas <philipp.jardas@qualifyze.com>
2022-11-01 14:34:38 +0100

    Submit coverage to CodeClimate

--- packages/core/jest.config.js
+++ packages/core/jest.config.js
@@ -1,13 +1,14 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/*.spec.ts'],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/*.spec.ts"],
+  coverageReporters: [["lcov", { projectRoot: "../.." }]],
   globals: {
-    'ts-jest': {
+    "ts-jest": {
       diagnostics: {
         ignoreCodes: [151001],
-      }
-    }
-  }
+      },
+    },
+  },
 };
